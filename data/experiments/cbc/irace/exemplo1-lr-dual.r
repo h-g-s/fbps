cb50e045cb0c2305b4020e8d3d3610e0d4af94db
Haroldo Gambini Santos <haroldo@ufop.edu.br>
2019-01-25 20:40:26 +0100

    irace settings

--- data/experiments/cbc/irace/exemplo1-lr-dual.r
+++
@@ -1,4 +0,0 @@
-library("irace")
-parameters <- readParameters("parameters-cbc-lr-dual.txt")
-scenario <- readScenario(filename = "scenarioCbc-lr-dual.txt", scenario = defaultScenario())
-irace(scenario = scenario, parameters = parameters)
--- data/experiments/cbc/irace/tunedual.sh
+++ data/experiments/cbc/irace/tunedual.sh
@@ -1,3 +1,3 @@
 iraceExe=/home/haroldo/R/x86_64-pc-linux-gnu-library/3.5/irace/bin/irace
 
-${iraceExe} --scenario dual-scenario.txt --max-time 10000 --parallel 7 --load-balancing=1
+${iraceExe} --scenario dual-scenario.txt --max-time 259000 --parallel 4 --load-balancing=1
