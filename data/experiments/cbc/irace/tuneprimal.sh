8bc9ff93b6f127fce63b447976631781c76768c5
Haroldo <haroldo.santos@gmail.com>
2019-03-15 10:18:24 +0100

    tune dual

---
+++ data/experiments/cbc/irace/tuneprimal.sh
@@ -0,0 +1,3 @@
+iraceExe=/home/haroldo/R/x86_64-pc-linux-gnu-library/3.5/irace/bin/irace
+
+${iraceExe} --scenario primal-scenario.txt --max-experiments 300000 --parallel 15 --test-type t-test --load-balancing=1
